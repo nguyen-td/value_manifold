38b725d2e56fdf2cf279134f4b01d8be44a8d664
Nguyen <nguyen.tiendung.nguyen@gmail.com>
2026-05-06 19:31:08 -0500

    add data

---
+++ analyze_behavior.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import numpy as np
+import torch
+from scipy.io import loadmat
+from sklearn.decomposition import PCA
+
+from perceptual_straightening.modules.elbo import ELBO
+
+# load data
+data_path = Path('data')
+save_params_path = Path('fitted_parameters')
+monkey_id = 1
+
+n_corr_obs = np.array(loadmat(Path(data_path) / f'n_corr_obs_monkey{monkey_id}.mat')['corr_offer_mat'], dtype=np.float32)
+n_total_obs = np.array(loadmat(Path(data_path) / f'n_total_obs_monkey{monkey_id}.mat')['total_offer_mat'], dtype=np.float32)
+n_bins = n_total_obs.shape[0]
+n_dim = n_bins - 1
+
+# run algorithm
+elbo = ELBO(n_dim, n_corr_obs, n_total_obs)
+c_est, p, _, _, _, _, _, _, _, _, _ = elbo.optimize_ELBO_SGD()
+
+# print results
+print(f'Estimated global curvature (estimated from most likely trajectory): {torch.rad2deg(torch.mean(c_est))} degrees')
+
+# save parameters
+out_pkl = Path(save_params_path) / f"value_manifold_monkey{monkey_id}_params.pkl"
--- construct_SU_matrix.m
+++ construct_SU_matrix.m
@@ -6,4 +6,68 @@ clear all
 close all
 clc
 
-%% Read data
+load('vmPFC_Trial_Vars_shared.mat') % load data
+
+%% Get range of subjective utility values for binning
+monkeys = {'Batman', 'Hobbes'};
+
+all_SU = {};
+offer_choices = {};
+subj_utilities = {};
+
+for imonkey = 1:numel(monkeys)
+    n_neurons = length(newTrial_Vars.subj_ID.(monkeys{imonkey}));
+    for ineuron = 1:n_neurons
+        vars = newTrial_Vars.subj_ID.(monkeys{imonkey}){ineuron}.vars;
+        subj_values = vars(:, 4:5);
+        all_SU{imonkey, ineuron} = subj_values(:);
+        subj_utilities{imonkey, ineuron} = vars(:, 4:5);
+
+        % store choices for constructing matrix
+        offer_choices{imonkey, ineuron} = vars(:, 1);
+    end
+end
+% h1 = histogram(vertcat(all_SU{1, :})); axis square, box off
+% h2 = histogram(vertcat(all_SU{2, :})); axis square, box off
+h = histogram(vertcat(all_SU{:})); axis square, box off
+bin_edges = h.BinEdges;
+
+% construct matrices
+monkey_id = 1;
+
+corr_offer_mat = zeros(numel(bin_edges), numel(bin_edges));
+total_offer_mat = zeros(numel(bin_edges), numel(bin_edges));
+
+for ineuron = 1:numel(newTrial_Vars.subj_ID.(monkeys{monkey_id})) % run for each monkey separately
+    su_bins = discretize(subj_utilities{monkey_id, ineuron}, bin_edges);
+    su1_minus_su2 = subj_utilities{monkey_id, ineuron}(:, 1) - subj_utilities{monkey_id, ineuron}(:, 2); % compute SU_1 - SU_2
+    for ibin = 1:size(su_bins, 1)
+        total_offer_mat(su_bins(ibin, 1), su_bins(ibin, 2)) = total_offer_mat(su_bins(ibin, 1), su_bins(ibin, 2)) + 1;
+
+        if (su1_minus_su2(ibin) > 0) && (offer_choices{monkey_id, ineuron}(ibin) == 1) % if SU_1 > SU_2 and choice = 1
+            corr_offer_mat(su_bins(ibin, 1), su_bins(ibin, 2)) = corr_offer_mat(su_bins(ibin, 1), su_bins(ibin, 2)) + 1;
+        elseif (su1_minus_su2(ibin) < 0) && (offer_choices{monkey_id, ineuron}(ibin) == 0) % if SU_1 < SU_2 and choice = 2
+            corr_offer_mat(su_bins(ibin, 1), su_bins(ibin, 2)) = corr_offer_mat(su_bins(ibin, 1), su_bins(ibin, 2)) + 1;
+        end
+    end
+end
+prop_corr_offer_mat = corr_offer_mat ./ total_offer_mat;
+
+%% Plot matrices
+% h = imagesc(bin_edges(1:50), bin_edges(1:50), prop_corr_offer_mat(1:50, 1:50)); 
+h = imagesc(bin_edges, bin_edges, prop_corr_offer_mat); 
+
+c = colorbar;
+c.Label.String = 'Proportion correct';
+title(['Monkey' num2str(monkey_id)], 'FontSize', 13)
+set(gca, 'YDir', 'normal', 'FontSize', 12);
+xlabel('Subjective utility offer 1')
+ylabel('Subjective utility offer 2')
+colormap gray
+axis square
+set(h, 'AlphaData', ~isnan(prop_corr_offer_mat)); % make nan transparent
+set(gca, 'Color', [0.8 0 0]); 
+
+%% Save matrices
+save(['data/n_corr_obs_monkey' num2str(monkey_id)], 'corr_offer_mat')
+save(['data/n_total_obs_monkey' num2str(monkey_id)], 'total_offer_mat')
