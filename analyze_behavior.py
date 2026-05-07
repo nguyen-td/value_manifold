18813545629dd115d25e5a7125d24ed82df73fc1
nguyen-td <tiendung.ngy@gmail.com>
2026-05-06 19:39:14 -0500

    add timer

--- analyze_behavior.py
+++ analyze_behavior.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 import torch
 from scipy.io import loadmat
+import time
 from sklearn.decomposition import PCA
 
 from perceptual_straightening.modules.elbo import ELBO
@@ -17,8 +18,11 @@ n_bins = n_total_obs.shape[0]
 n_dim = n_bins - 1
 
 # run algorithm
+t = time.perf_counter
 elbo = ELBO(n_dim, n_corr_obs, n_total_obs)
 c_est, p, _, _, _, _, _, _, _, _, _ = elbo.optimize_ELBO_SGD()
+elapsed = time.perf_counter() - t
+print(f'Finished in {elapsed:.2f} sec')
 
 # print results
 print(f'Estimated global curvature (estimated from most likely trajectory): {torch.rad2deg(torch.mean(c_est))} degrees')
