93d05f682450254ab6b5c482752328bc68395cd1
Nguyen <nguyen.tiendung.nguyen@gmail.com>
2026-05-16 09:12:02 -0400

    add params

--- analyze_behavior.py
+++ analyze_behavior.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from scipy.io import loadmat
 import time
+import pickle
 from sklearn.decomposition import PCA
 
 from perceptual_straightening.modules.elbo import ELBO
@@ -10,22 +11,22 @@ from perceptual_straightening.modules.elbo import ELBO
 # load data
 data_path = Path('data')
 save_params_path = Path('fitted_parameters')
-monkey_id = 1
+monkey_id = 2
+n_bin_edges = 10
 
-n_corr_obs = np.array(loadmat(Path(data_path) / f'n_corr_obs_monkey{monkey_id}.mat')['corr_offer_mat'], dtype=np.float32)
-n_total_obs = np.array(loadmat(Path(data_path) / f'n_total_obs_monkey{monkey_id}.mat')['total_offer_mat'], dtype=np.float32)
+n_corr_obs = np.array(loadmat(Path(data_path) / f'n_corr_obs_monkey{monkey_id}_{n_bin_edges}bins.mat')['corr_offer_mat'], dtype=np.float32)
+n_total_obs = np.array(loadmat(Path(data_path) / f'n_total_obs_monkey{monkey_id}_{n_bin_edges}bins.mat')['total_offer_mat'], dtype=np.float32)
 n_bins = n_total_obs.shape[0]
 n_dim = n_bins - 1
 
 # run algorithm
-t = time.perf_counter
 elbo = ELBO(n_dim, n_corr_obs, n_total_obs)
 c_est, p, _, _, _, _, _, _, _, _, _ = elbo.optimize_ELBO_SGD()
-elapsed = time.perf_counter() - t
-print(f'Finished in {elapsed:.2f} sec')
 
 # print results
 print(f'Estimated global curvature (estimated from most likely trajectory): {torch.rad2deg(torch.mean(c_est))} degrees')
 
 # save parameters
-out_pkl = Path(save_params_path) / f"value_manifold_monkey{monkey_id}_params.pkl"
+out_pkl = Path(save_params_path) / f"value_manifold_monkey{monkey_id}_{n_bins}bins_params.pkl"
+with open(out_pkl, 'wb') as file:
+    pickle.dump(elbo, file)
